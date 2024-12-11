92c9ca12bbdf9f97434bc4c9885af25b57e4cac7
arasu20 <imarasu20@gmail.com>
2024-12-11 15:25:47 +0530

    created functions for cart

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,19 @@
 export const cart =[
     
 ];
+export function addToCart(productId){
+    let sameProduct;
+         cart.forEach((cartItem)=>{
+            if(productId === cartItem.productId){
+                sameProduct = cartItem;
+            }
+         });
+         if(sameProduct){
+            sameProduct.quantity +=1;
+         }else{
+            cart.push({
+                productId: productId,
+                quantity: 1
+             });
+         }
+}
