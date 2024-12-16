a5a325da650f434376f6e3bbaef8976d4356b20b
arasu20 <imarasu20@gmail.com>
2024-12-16 23:28:55 +0530

    regenerating html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,9 @@ import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions } from "../data/deliveryOptions.js";
 
+function renderAgain(){
+
+
 let cartHTML = '';
 
 cart.forEach((cartItem)=>{
@@ -118,6 +121,10 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 document.querySelectorAll('.js-delivery-option').forEach((element)=>{
     element.addEventListener('click',()=>{
         const {productId, deliveryOptionId}= element.dataset;
-        updateDeliveryOption(productId, deliveryOptionId)
+        updateDeliveryOption(productId, deliveryOptionId);
+        renderAgain();
     });
 })
+}
+
+renderAgain();
