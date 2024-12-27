67456914b8328bb060598106e76a6505f0c3b206
arasu20 <imarasu20@gmail.com>
2024-12-27 18:03:35 +0530

    created order js

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, updateCartQuantity } from "../../data/cart.js";
 import { productsInCart } from "../../data/products.js";
 import { getDeliveryOptionID } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 export function renderPayment(){
     let costOfProducts = 0;
     let shippingPrice = 0;
@@ -9,8 +10,8 @@ export function renderPayment(){
         const product = productsInCart(cartItem.productId);
         costOfProducts += product.priceCents*cartItem.quantity;
 
-       const deliveryOption =  getDeliveryOptionID(cartItem.deliveryOptionId)
-       shippingPrice+=deliveryOption.priceCents
+      const deliveryOption =  getDeliveryOptionID(cartItem.deliveryOptionId)
+      shippingPrice+=deliveryOption.priceCents
 
     });
     const totalBeforeTax = costOfProducts + shippingPrice;
@@ -46,9 +47,29 @@ export function renderPayment(){
             <div class="payment-summary-money">$${formatCurrency(totalPrice)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button js-place-order button-primary">
             Place your order
           </button>`;
 
           document.querySelector(".js-payment-summary").innerHTML=paymentHTML;
+
+          document.querySelector('.js-place-order').addEventListener('click',async ()=>{
+            try{
+              const response = await fetch('https://supersimple.dev/orders',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  cart: cart
+                })
+              });
+              const order = await response.json();
+              addOrder(order);
+            }
+            catch(error){
+              console.log(error, 'Try again later');
+            }
+            window.location.href = 'orders.html';
+          })
 }
